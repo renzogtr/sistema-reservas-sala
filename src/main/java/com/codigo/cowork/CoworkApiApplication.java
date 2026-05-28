844f1a8729d5f5ebd875b215616824bf6875aedc
renzogtr <torresrojas2009@gmail.com>
2026-05-27 20:28:15 -0500

    renombrar package com.codigo.cowork_api a com.codigo.cowork

--- src/main/java/com/codigo/cowork/CoworkApiApplication.java
+++ src/main/java/com/codigo/cowork/CoworkApiApplication.java
@@ -1,4 +1,4 @@
-package com.codigo.cowork_api;
+package com.codigo.cowork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/codigo/cowork/CoworkApiApplicationTests.java
+++ src/test/java/com/codigo/cowork/CoworkApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.codigo.cowork_api;
+package com.codigo.cowork;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
