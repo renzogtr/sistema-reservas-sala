c26b3a3ac70356a5e23b3cafd3a7abf80fd77c2a
renzogtr <torresrojas2009@gmail.com>
2026-05-27 19:34:01 -0500

    init: estructura base del proyecto Spring Boot

---
+++ src/main/java/com/codigo/sistema_reservas_sala/SistemaReservasSalaApplication.java
@@ -0,0 +1,13 @@
+package com.codigo.sistema_reservas_sala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaReservasSalaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaReservasSalaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codigo/sistema_reservas_sala/SistemaReservasSalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.sistema_reservas_sala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaReservasSalaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
